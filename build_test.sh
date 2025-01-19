bfff3f9ba7e0fb64dacb6b4688730c6e485188a7
decimatedTomato <greentomatopower@gmail.com>
2025-01-19 15:52:48 +0100

    Tried a lot of things that didn't work

--- build_test.sh
+++ build_test.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-set -xe
-
 if [[ $1 == "clean" ]]; then
     rm -rf src/build/
     rm -rf test/build/
     # cmake --build C:/foo/build/ --target clean
 fi
 
+set -xe
 mkdir -p src/build
 mkdir -p test/build
 cd test/build
 cmake .. -DCMAKE_BUILD_TYPE=Debug
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    make
-    cd -
-    ./test/build/test_tomato_juice
-elif [[ "$OSTYPE" == "msys" ]]; then
-    devenv.com test_tomato_juice.sln -Build
-    cd -
-    ./test/build/Debug/test_tomato_juice
-else
-    exit 1
+if [[ $1 == "run" ]] || [[ $2 == "run" ]]; then
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        make
+        cd -
+        ./test/build/test_tomato_juice
+    elif [[ "$OSTYPE" == "msys" ]]; then
+        devenv.com test_tomato_juice.sln -Build
+        cd -
+        ./test/build/Debug/test_tomato_juice
+    else
+        exit 1
+    fi
 fi
 
-# TODO add valgrind check and python integration tests
+# TODO add python integration tests
--- test/string/test_s8.cpp
+++ test/string/test_s8.cpp
@@ -3,19 +3,30 @@ extern "C"
 #include "tomato_string.h"
 }
 
+// #if defined(__clang__) || defined(__GNUC__)
+// #define ATTRIBUTE_NO_SANITIZE_ADDRESS __attribute__((no_sanitize_address))
+// #else
+// #define ATTRIBUTE_NO_SANITIZE_ADDRESS
+// #endif
 #include <gtest/gtest.h>
 #include <string.h>
 
+/* Should there really be a unit test that takes this white box approach?
+ * This test will fail if I ever change the underlying memory layout, even if that is an intentional change
+ */
+// __attribute__((no_sanitize_address))
 TEST(S8_init, Underlying_data)
 {
     const usize allocated_len = 64;
-    const s8    str = s8_init(allocated_len);
+    const s8    str_empty = s8_init(allocated_len);
     const char  data[allocated_len]{};
-    EXPECT_FALSE(!str);
-    EXPECT_EQ(memcmp(str, data, allocated_len), 0);
-    EXPECT_EQ(*(reinterpret_cast<size_t *>(str) - 1), allocated_len);
+    EXPECT_FALSE(!str_empty);
+    EXPECT_EQ(memcmp(str_empty, data, allocated_len), 0);
+    EXPECT_EQ(*(reinterpret_cast<size_t *>(str_empty) - 1), allocated_len);
+    s8_free(str_empty);
 }
 
+// __attribute__((no_sanitize_address))
 TEST(S8_free, Memory_zeroed)
 {
     const s8 poem =
@@ -23,8 +34,8 @@ TEST(S8_free, Memory_zeroed)
            "hour.\nThen leaf subsides to leaf.\nSo Eden sank to grief,\nSo dawn goes down to day.\nNothing gold can "
            "stay.\n");
     const char   data[sizeof(poem)]{};
-    const usize *poem_len = (reinterpret_cast<size_t *>(poem) - 1);
+    const usize *poem_len = reinterpret_cast<usize *>(poem) - 1;
     s8_free(poem);
-    // EXPECT_EQ(*poem_len, 0); // FIXME use after free?
+    EXPECT_EQ(*poem_len, 0);
     // EXPECT_EQ(memcmp(poem, data, sizeof(poem)), 0);
 }
