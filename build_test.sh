4e4501d742a7ad3a501c0c635c90b959d8877b10
decimatedTomato <greentomatopower@gmail.com>
2025-01-20 15:27:39 +0100

    No clue what I was cooking

--- build_test.sh
+++ build_test.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
+# set -xe
+
 if [[ $1 == "clean" ]]; then
     rm -rf src/build/
     rm -rf test/build/
     # cmake --build C:/foo/build/ --target clean
 fi
 
-set -xe
 mkdir -p src/build
 mkdir -p test/build
 cd test/build
 cmake .. -DCMAKE_BUILD_TYPE=Debug
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    make
+    cd -
+elif [[ "$OSTYPE" == "msys" ]]; then
+    devenv.com test_tomato_juice.sln -Build
+    cd -
+else
+    exit 1
+fi
+
 if [[ $1 == "run" ]] || [[ $2 == "run" ]]; then
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        make
-        cd -
         ./test/build/test_tomato_juice
     elif [[ "$OSTYPE" == "msys" ]]; then
-        devenv.com test_tomato_juice.sln -Build
-        cd -
         ./test/build/Debug/test_tomato_juice
     else
         exit 1
