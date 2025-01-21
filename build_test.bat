592863180ecd36c55562db820500efa50fc75169
decimatedTomato <greentomatopower@gmail.com>
2025-01-21 12:29:25 +0100

    Windows compilation has been fixed

--- build_test.bat
+++ build_test.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 
+@REM https://devblogs.microsoft.com/cppblog/asan-for-windows-x64-and-debug-build-support/
+
 if "%1"=="clean" (
     IF EXIST "src\build" RD "src\build" /S /Q
     IF EXIST "test\build" RD "test\build" /S /Q
@@ -8,11 +10,10 @@ if "%1"=="clean" (
 
 IF NOT EXIST "src\build" MD "src\build"
 IF NOT EXIST "test\build" MD "test\build"
-@REM MD src\build
-@REM MD test\build
 
 WHERE /q cl || CALL "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 cmake -S test -B test\build || EXIT /B 1
-devenv test\build\test_tomato_juice.sln /Build && EXIT /B 1
+devenv test\build\test_tomato_juice.sln /Build || EXIT /B 1
 
-IF "$1"=="run" CALL test\build\Debug\test_tomato_juice.exe
+echo "%1"
+IF "%1"=="run" CALL test\build\Debug\test_tomato_juice.exe
