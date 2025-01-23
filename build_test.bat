da9b41647626c828dfcf4e2ae333ff689bcd47a8
decimatedTomato <greentomatopower@gmail.com>
2025-01-23 18:11:58 +0100

    [build] Tried to change compiler to clang-cl
    The reason for this was to be able to surpress asan on certain functions
    which is a feature of certain compilers that use asan on linux.
    Since the compiler still uses cl I'm not sure what exactly
    clang-cl accomplishes.
    I couldn't get it to compile properly though so, whatever.

--- build_test.bat
+++ build_test.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 
+@REM https://learn.microsoft.com/en-us/cpp/sanitizers/asan?view=msvc-170#command-prompt 
+@REM https://devblogs.microsoft.com/cppblog/addresssanitizer-asan-for-windows-with-msvc/
 @REM https://devblogs.microsoft.com/cppblog/asan-for-windows-x64-and-debug-build-support/
 
 if "%1"=="clean" (
@@ -11,9 +13,9 @@ if "%1"=="clean" (
 IF NOT EXIST "src\build" MD "src\build"
 IF NOT EXIST "test\build" MD "test\build"
 
-WHERE /q cl || CALL "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-cmake -S test -B test\build || EXIT /B 1
-devenv test\build\test_tomato_juice.sln /Build || EXIT /B 1
+WHERE /Q cl || CALL "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+cmake -T Clangcl -S test -B test\build -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang || EXIT /B 1
+cmake --build test\build --config Release
+@REM devenv test\build\test_tomato_juice.sln /Build || EXIT /B 1
 
-echo "%1"
-IF "%1"=="run" CALL test\build\Debug\test_tomato_juice.exe
+@REM IF "%1"=="run" CALL test\build\Debug\test_tomato_juice.exe
--- test/string/test_s8.cpp
+++ test/string/test_s8.cpp
@@ -3,18 +3,19 @@ extern "C"
 #include "tomato_string.h"
 }
 
-// #if defined(__clang__) || defined(__GNUC__)
-// #define ATTRIBUTE_NO_SANITIZE_ADDRESS __attribute__((no_sanitize_address))
-// #else
-// #define ATTRIBUTE_NO_SANITIZE_ADDRESS
-// #endif
+// https://github.com/google/sanitizers/wiki/AddressSanitizer/000d3f9655b81a6f661277d3ab8716826a237309
+#if defined(__clang__) || defined(__GNUC__)
+#define ATTRIBUTE_NO_SANITIZE_ADDRESS __attribute__((no_sanitize_address))
+#else
+#define ATTRIBUTE_NO_SANITIZE_ADDRESS
+#endif
+
 #include <gtest/gtest.h>
 #include <string.h>
 
 /* Should there really be a unit test that takes this white box approach?
  * This test will fail if I ever change the underlying memory layout, even if that is an intentional change
  */
-// __attribute__((no_sanitize_address))
 TEST(S8_init, Underlying_data)
 {
     const usize allocated_len = 64;
@@ -26,16 +27,34 @@ TEST(S8_init, Underlying_data)
     s8_free(str_empty);
 }
 
-// __attribute__((no_sanitize_address))
-TEST(S8_free, Memory_zeroed)
+/* Try doing this in a setup fixture struct */
+// auto dangerous_operation(s8 const &str, const usize str_size) -> void
+// {
+//     const usize *str_len = reinterpret_cast<usize *>(str) - 1;
+//     const char   data[str_size]{};
+//     s8_free(str);
+//     EXPECT_EQ(*str_len, 0);
+//     EXPECT_EQ(memcmp(str, data, str_size), 0);
+// }
+struct S8_free : public testing::Test
 {
+  protected:
+    ATTRIBUTE_NO_SANITIZE_ADDRESS
+    void TearDown() override
+    {
+        EXPECT_EQ(*poem_len, 0);
+        EXPECT_EQ(memcmp(poem, data, 205), 0);
+    }
     const s8 poem =
         S8("Nature's first green is gold,\nHer hardest hue to hold.\nHer early leaf's a flower;\nBut only so an "
            "hour.\nThen leaf subsides to leaf.\nSo Eden sank to grief,\nSo dawn goes down to day.\nNothing gold can "
            "stay.\n");
-    const char   data[sizeof(poem)]{};
     const usize *poem_len = reinterpret_cast<usize *>(poem) - 1;
+    const char   data[204]{};
+};
+TEST_F(S8_free, Memory_zeroed)
+{
+    EXPECT_EQ(*poem_len, 203);
+    EXPECT_NE(memcmp(poem, data, 204), 0);
     s8_free(poem);
-    EXPECT_EQ(*poem_len, 0);
-    // EXPECT_EQ(memcmp(poem, data, sizeof(poem)), 0);
 }
