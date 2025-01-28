a1d5b26ea161496e0fb2717b4bab799528f366e7
decimatedTomato <greentomatopower@gmail.com>
2025-01-28 20:13:37 +0100

    [build] Improved windows build script

--- build_test.bat
+++ build_test.bat
@@ -1,6 +1,4 @@
-@REM @ECHO OFF
-
-@REM https://devblogs.microsoft.com/cppblog/asan-for-windows-x64-and-debug-build-support/
+@ECHO OFF
 
 if "%1"=="clean" (
     IF EXIST "src\build" RD "src\build" /S /Q
@@ -11,8 +9,7 @@ IF NOT EXIST "src\build" MD "src\build"
 IF NOT EXIST "test\build" MD "test\build"
 
 WHERE /Q cl || CALL "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-cmake -S test -B test\build --log-level=ERROR || EXIT /B 1
-devenv test\build\test_tomato_juice.sln /Build || EXIT /B 1
-@REM cmake --build --config BUILD
+cmake -S test -B test\build || EXIT /B 1
+cmake --build test\build --config Debug || EXIT /B 1
 
 IF "%1"=="run" CALL test\build\Debug\test_tomato_juice.exe
